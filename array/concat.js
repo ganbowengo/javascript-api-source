623309d3bbfa1477e1208ed879091a63f04a1506
“ganbowengo” <1245960973@qq.com>
2019-08-20 22:06:59 +0800

    完成concat的挨批编写

--- array/concat.js
+++ array/concat.js
@@ -1,6 +1,27 @@
 // 数组的concat方法实现
 
-export function concat(arr, args) {
-    
-    return
+Array.prototype.concat1 = function () {
+    let arr = this.slice()
+    console.log('arguments',arguments)
+    for (let key in arguments) {
+        if (arguments[key] instanceof Array) {
+            console.log(arguments[key])
+            arguments[key].forEach(element => {
+                arr[arr.length] = element
+            });
+        } else {
+            arr[arr.length] = arguments[key]
+        }
+    }
+    return arr
 }
+
+console.log([1, 2, 3].concat1([5, 6], 1))
+/**
+ * 遇到问题
+ * 1.arguments在forEach的时候不能遍历 
+ *   答案： arguments是以0，1，2，3...为key的Object
+ * 2.for in 会将数组__proto__中自己添加的方法遍历出来
+ * 3.怎么设置一个原型上的函数为不可枚举
+ * 
+ */

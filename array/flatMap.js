e70983b7fdec7037d6fb5f8d833b1de2281a4b57
“ganbowengo” <1245960973@qq.com>
2019-08-24 15:58:14 +0800

    完成flatmap

--- array/flatMap.js
+++ array/flatMap.js
@@ -1,1 +1,31 @@
-undefined
+Array.prototype.flatMap1 = function (callbackFn /* ,thisArg */ ) {
+    if (this === null) {
+        throw new TypeError('this is null or defined')
+    }
+    if (typeof callbackFn !== 'function') {
+        throw new TypeError('predicate must be a function')
+    }
+    let O = Object(this)
+    let len = O.length >>> 0
+    let i = 0
+    let thisArg = null
+    let result = []
+    if (arguments[1]) {
+        thisArg = arguments[1]
+    }
+    let item = null
+    while (i < len) {
+        item = callbackFn.call(thisArg, O[i], i, O)
+        if (item instanceof Array) {
+            result = result.concat(item)
+        } else {
+            result.push(item)
+        }
+        i++
+    }
+    return result
+}
+
+let a = [1, 2, 3, 4]
+console.log(a.flatMap1(item => [[item * 2]]))
+console.log(a)

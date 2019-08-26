424fc2df6ad2680ea3c48dde17fdc80bbcab16df
“ganbowengo” <1245960973@qq.com>
2019-08-26 17:20:51 +0800

    完成map\ of

--- array/map.js
+++ array/map.js
@@ -1,1 +1,20 @@
-undefined
+// map() 方法创建一个新数组，其结果是该数组中的每个元素都调用一个提供的函数后返回的结果。
+Array.prototype.map1 = function (callbackFn /* ,thisArg */ ) {
+    if (this == null) {
+        throw new TypeError('this is null or defined')
+    }
+    if(typeof callbackFn !== 'function'){
+        throw new TypeError('Array map requires an callback function - not null or defind')
+    }
+    let O = Object(this)
+    let len = O.length >>> 0
+    let thisArg = arguments.length > 1 ? arguments[1] : null
+    let k = 0
+    let R = new Array()
+    while (k < len) {
+        R[k++] = callbackFn.call(thisArg, O[k], k, O)
+    }
+    return R
+}
+
+console.log([1,2,4,5].map(item => item * 2))
--- array/of.js
+++ array/of.js
@@ -1,1 +1,21 @@
-undefined
+// Array.of() 方法创建一个具有可变数量参数的新数组实例，而不考虑参数的数量或类型。
+// Array.of() 和 Array 构造函数之间的区别在于处理整数参数：Array.of(7) 创建一个具有单个元素 7 的数组，
+// 而 Array(7) 创建一个长度为7的空数组（注意：这是指一个有7个空位(empty)的数组，而不是由7个undefined组成的数组）。
+Array.of1 = function(){
+    let O = new Array()
+    let len = arguments.length
+    let k = 0
+    while(k < len){
+        O[k] = arguments[k]
+        k++
+    }
+    return O
+}
+
+/**
+ * 
+ * 注意问题
+ * 1. 可以使用slice直接将类数组的arguments 转为数组
+ *    return Array.prototype.slice.call(arguments)
+ * 
+ */

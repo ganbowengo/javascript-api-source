147e3f181b62cb45d41bdea6d052e83ef06c7118
“ganbowengo” <1245960973@qq.com>
2019-08-26 15:23:57 +0800

    完成isArray

--- array/isArray.js
+++ array/isArray.js
@@ -1,1 +1,16 @@
-undefined
+// Array.isArray() 用于确定传递的值是否是一个 Array。
+Array.isArray1 = function (target) {
+    return Object.prototype.toString.call(target) === '[object Array]'
+}
+
+/**
+ * 注意问题 
+ * 不同于 typeof [] 返回的是 object
+ * instanceof 比较的是prototype是否相同 可以伪造出数组
+ * 
+ */
+
+ let a = []
+ console.log(a instanceof Array)
+ a.__proto__ = Number.prototype
+ console.log(a instanceof Array)

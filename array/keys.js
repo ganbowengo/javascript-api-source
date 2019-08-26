fe97f1c00a38bd3e2ecbb1d015d66e6184039699
“ganbowengo” <1245960973@qq.com>
2019-08-26 16:31:55 +0800

    完成keys

--- array/keys.js
+++ array/keys.js
@@ -1,1 +1,37 @@
-undefined
+Array.prototype.keys1 = function () {
+    if (this == null) {
+        throw new TypeError('this is null or defined')
+    }
+    let O = Object(this)
+    let len = O.length
+    let index = -1
+    return {
+        next: function () {
+            index++
+            return index < len ? {
+                value: index,
+                done: false
+            } : {
+                value: undefined,
+                done: true
+            }
+        },
+        [Symbol.iterator]: function () { //部署遍历器生成方法
+            return this
+        }
+    }
+}
+
+let a = [1, 2, 3, 4].keys1()
+for (let i of a) {
+    console.log(i)
+}
+// console.log(a.next())
+// console.log(a.next())
+// console.log(a.next())
+// console.log(a.next())
+// console.log(a.next())
+/**
+ * 注意问题
+ * 生成一个iterator 需要部署遍历器的生成方法[Symbol.iterator]
+ */

1257cbd9c4ae1c728fd0526eb93b1f603c8da313
“ganbowengo” <1245960973@qq.com>
2019-08-27 14:36:11 +0800

    提交reduce reduceRight

--- array/reduce.js
+++ array/reduce.js
@@ -1,1 +1,36 @@
-undefined
+// reduce() 方法对数组中的每个元素执行一个由您提供的reducer函数(升序执行)，将其结果汇总为单个返回值。
+Array.prototype.reduce1 = function (callbackFn /* ,initalValue */ ) {
+    if (this == null) {
+        throw new TypeError('this is null or defined')
+    }
+    if (typeof callbackFn !== 'function') {
+        throw new TypeError(callbackFn + 'not a function')
+    }
+    let O = Object(this)
+    let len = O.length >>> 0
+    let initalValue = arguments.length > 1 ? arguments[1] : O[0]
+    if(initalValue === undefined && len === 0){
+        throw new TypeError('Reduce of empty array with no inital value')
+    }
+    let k = len > 0 && arguments.length === 1 ? 0 : -1
+    k++
+    while(k < len){
+        if(k in O){
+            initalValue = callbackFn(initalValue, O[k], k, O)
+        }
+        k++
+    }
+    return initalValue
+}
+
+console.log([1,2,3,4].reduce((item,item1) => item + item1,5))
+console.log([1,2,3,4].reduce1((item,item1) => item + item1,5))
+
+/**
+ * 
+ * 注意问题 
+ * 1. 只能专递两个参数
+ *  回调函数 、 初始值
+ * 2. 未传递第二个参数时 以数组的第一个元素为初始值 并从第二个元素开始调用回调函数
+ * 
+ */
--- array/reduceRight.js
+++ array/reduceRight.js
@@ -1,1 +1,37 @@
-undefined
+// reduceRight() 方法接受一个函数作为累加器（accumulator）和数组的每个值（从右到左）将其减少为单个值。
+Array.prototype.reduceRight1 = function (callbackFn /* initalValue */ ) {
+    if (this == null) {
+        throw new TypeError('this is null or defined')
+    }
+    if (typeof callbackFn !== 'function') {
+        throw new TypeError(callbackFn + 'not a function')
+    }
+    let O = Object(this)
+    let len = O.length >>> 0
+    let initalValue = arguments.length > 1 ? arguments[1] : O[len-1]
+    if (initalValue === undefined && len === 0) {
+        throw new TypeError('Reduce of empty array with no inital value')
+    }
+    let k = len > 0 && arguments.length === 1 ? len - 1 : len
+    k--
+    while (k >= 0) {
+        if (k in O) {
+            console.log(initalValue)
+            initalValue = callbackFn(initalValue, O[k], k, O)
+        }
+        k--
+    }
+    return initalValue
+}
+const array1 = [[0, 1], [2, 3], [4, 5]].reduceRight1(
+    (accumulator, currentValue) => accumulator.concat(currentValue)
+);
+console.log(array1);
+
+/**
+ * 注意问题
+ * 1. 与reduce的功能与参数相同 从后向前叠加
+ */
+[{vId: 1, aId: 1},{vId: 1, aId: 2},{vId: 2, aId: 1}].reduce((sum,current)=>{
+    
+},[])

6355c5ad5efd9d9a87de228b8ca622994eeb641c
“ganbowengo” <1245960973@qq.com>
2019-09-02 09:54:10 +0800

    完成defineProperties

--- object/defineProperties.js
+++ object/defineProperties.js
@@ -1,1 +1,52 @@
-undefined
+// Object.defineProperties() 方法直接在一个对象上定义新的属性或修改现有属性，并返回该对象。
+Object.defineProperties = function (obj, properties) {
+    function convertToDescriptor(desc) {
+        function hasProperty(obj, prop) {
+            return Object.prototype.hasOwnProperty.call(obj, prop);
+        }
+
+        function isCallable(v) {
+            return typeof v === 'function';
+        }
+        if (typeof obj !== 'object' || obj === null)
+            throw new TypeError('desc is null or dedined');
+        let d = {};
+        if (hasProperty(desc, 'enumerable'))
+            d.enumerable = !!desc.enumerable;
+        if (hasProperty(desc, 'configurable'))
+            d.configurable = !!desc.configurable;
+        if (hasProperty(desc, 'value'))
+            d.value = desc.value;
+        if (hasProperty(desc, 'writable'))
+            d.writable = !!desc.writable;
+        if (hasProperty(desc, 'get')) {
+            var g = desc.get;
+
+            if (!isCallable(g) && typeof g !== 'undefined')
+                throw new TypeError('bad get');
+            d.get = g;
+        }
+        if (hasProperty(desc, 'set')) {
+            var s = desc.set;
+            if (!isCallable(s) && typeof s !== 'undefined')
+                throw new TypeError('bad set');
+            d.set = s;
+        }
+        if (('get' in d || 'set' in d) && ('value' in d || 'writable' in d))
+            throw new TypeError('identity-confused descriptor');
+
+        return d;
+    }
+    if (typeof obj !== 'object' || obj === null)
+        throw new TypeError('bad obj');
+    properties = Object(properties);
+    let keys = Object.keys(properties);
+    let descs = [];
+    for (let i = 0; i < keys.length; i++)
+        descs.push([keys[i], convertToDescriptor(properties[keys[i]])]);
+
+    for (let i = 0; i < descs.length; i++)
+        Object.defineProperty(obj, descs[i][0], descs[i][1]);
+
+    return obj;
+}
